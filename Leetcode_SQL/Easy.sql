5a04f5e2e40d9f9be392dfda94f91b68746700e3
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-04-30 18:58:17 +0530

    Update Easy.sql

--- Leetcode_SQL/Easy.sql
+++ Leetcode_SQL/Easy.sql
@@ -1,1 +1,13 @@
 
+-- Day 1 | Question 1
+-- LeetCode #175: Combine Two Tables
+-- Difficulty: Easy
+-- Topic: LEFT JOIN
+
+SELECT p.firstName,
+       p.lastName,
+       a.city,
+       a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
